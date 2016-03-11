542e52b3d8bc4dcac27d034199c41127c13b9fff
Clint Ascencio <clint.ascencio@datastax.com>
2016-03-11 14:08:03 -0600

    Generic Models

---
+++ models.py
@@ -0,0 +1,48 @@
+from flask import Flask
+
+class People():
+    id = None
+    name = None
+    height = None
+    hair_color = None
+    skin_color = None
+    eye_color = None
+    birth_year = None
+    gender = None
+    homeworld = None
+    films = None
+    species = None
+    vehicles = None
+    starships = None
+    url = None
+
+
+class Planets():
+    id = None
+    name = None
+    rotation_period = None
+    diameter = None
+    climate = None
+    gravity = None
+    terrain = None
+    surface_water = None
+    population = None
+    residents = None
+    films = None
+    url = None
+
+class Species():
+    id = None
+    name = None
+    classification = None
+    designation = None
+    average_height = None
+    skin_colors = None
+    hair_colors = None
+    eye_colors = None
+    average_lifespan = None
+    homeworld = None
+    language = None
+    people = None
+    films = None
+    url = None

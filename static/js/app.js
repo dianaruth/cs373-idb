eac62ba3de577eab3d0ea25c718c86b52bd31a5c
Diana Ruth <dianaruth40@gmail.com>
2016-03-21 18:47:27 -0500

    almost getting partials loading

--- static/js/app.js
+++ static/js/app.js
@@ -1,19 +1,21 @@
-/* Main Application */
-
 var returnOfTheAPI = angular.module('returnOfTheAPI', ['controllers', 'ngRoute']);
 
-returnOfTheAPI.config(['$routeProvider',
-    function($routeProvider){
+returnOfTheAPI.config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider){
         $routeProvider.
             when('/', {
-                templateUrl : '../static/partials/home.html',
+                templateUrl : '/static/partials/home.html',
                 controller : 'HomeController'
             }).
             when('/about', {
-                templateUrl : '../static/partials/about.html',
+                templateUrl : '/static/partials/about.html',
                 controller : 'AboutController'
             }).
             otherwise({
                 redirectTo: '/'
             });
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
 }]);
--- static/js/controllers.js
+++ static/js/controllers.js
@@ -5,4 +5,9 @@ angular.module('controllers', [])
     }])
     .controller('AboutController', ['$scope' , function($scope) {
 
+    }])
+    .controller('NavController', ['$scope', '$location' , function($scope, $location) {
+        $scope.isActive = function(viewLocation) {
+            return $location.path().indexOf(viewLocation) == 0;
+        }
     }]);

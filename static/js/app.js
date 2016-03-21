f49a4125c3a5eda409f52ab57640efb73be78fea
Diana Ruth <dianaruth40@gmail.com>
2016-03-21 16:57:55 -0500

    having issues with angular routing

--- static/js/app.js
+++ static/js/app.js
@@ -13,25 +13,6 @@ returnOfTheAPI.config(['$routeProvider',
                 templateUrl : '../static/partials/about.html',
                 controller : 'AboutController'
             }).
-//            when('/albums', {
-//                templateUrl : '/partials/albums.html',
-//                controller : 'AlbumTableCtrl',
-//                css : 'css/albums.css'
-//            }).
-//            when('/tracks', {
-//                templateUrl : '/partials/tracks.html',
-//                controller : 'TrackTableCtrl',
-//                css : 'css/tracks.css'
-//            }).
-//            when('/test/:artistID', {
-//                templateUrl : '/partials/artist-details.html',
-//                controller : 'ArtistDetailsCtrl'
-//            }).
-//            when('/about', {
-//                templateUrl : 'partials/about.html',
-//                controller : 'AboutCtrl',
-//                css : 'css/about.css'
-//            }).
             otherwise({
                 redirectTo: '/'
             });

b22a90d1ecf883a978ddacf7fbedf031315eb376
Clint Ascencio <clint.ascencio@datastax.com>
2016-03-24 16:41:01 -0500

    Added models.py documentation

--- models.py
+++ models.py
@@ -1,26 +1,43 @@
-from flask import Flask
+from flask import *
 from sqlalchemy import *
 
+# ----
+# People
+# ----
 class People():
+    """"People Model"""
+
     __tablename__ = 'people'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
+    gender = Column(String)
+    birth_year = Column(String)
     height = Column(Integer)
     mass = Column(Integer)
     hair_color = Column(String)
-    skin_color = Column(String)
     eye_color = Column(String)
-    birth_year = Column(String)
-    gender = Column(String)
-    homeworld = Column(String)
-    films = Column(String)
-    species = Column(String)
-    vehicles = Column(String)
-    starships = Column(String)
-    url = Column(String)
 
+    
     def __init__(self, name, gender, birth_year, height, mass, hair_color, eye_color):
+        """
+        Assigns variables to the class upon initialization.
+
+        Parameters
+        ----------
+        self : Planets
+        name : Column(String)
+        gender : Column(String)
+        birth_year : Column(String)
+        height: Column(Integer)
+        mass : Column(Integer)
+        hair_color : Column(String)
+        eye_color : Column(String)
+
+        Returns
+        -------
+        People
+        """
         self.name = name
         self.gender = gender
         self.birth_year = birth_year
@@ -29,49 +46,77 @@ class People():
         self.hair_color = hair_color
         self.eye_color = eye_color
 
-
+# ----
+# Planets
+# ----
 class Planets():
+    """Planets Model"""
+
     __tablename__ = 'planets'
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
-    rotation_period = Column(String)
-    diameter = Column(String)
     climate = Column(String)
     gravity = Column(String)
     terrain = Column(String)
-    surface_water = Column(String)
     population = Column(Integer)
-    residents = Column(String)
-    films = Column(String)
-    url = Column(String)
-
+    
     def __init__(self, name, climate, gravity, terrain, population): 
+        """
+        Assigns variables to the class upon initialization.
+
+        Parameters
+        ----------
+        self : Planets
+        name : Column(String)
+        climate : Column(String)
+        gravity : Column(String)
+        terrain : Column(String)
+        population : Column(Integer)
+
+        Returns
+        -------
+        Planets
+        """
         self.name = name
         self.climate = climate
         self.gravity = gravity
         self.terrain = terrain
         self.population = population
 
+# ----
+# Species
+# ----
 class Species():
+    """Species Model"""
+
     __tablename__ = 'species'
 
+    #
     id = Column(Integer, primary_key=True)
     name = Column(String)
     classification = Column(String)
-    designation = Column(String)
     average_height = Column(String)
-    skin_colors = Column(String)
-    hair_colors = Column(String)
-    eye_colors = Column(String)
     average_lifespan = Column(String)
-    homeworld = Column(String)
     language = Column(String)
-    people = Column(String)
-    films = Column(String)
-    url = Column(String)
 
     def __init__(self, name, classification, average_height, average_lifespan, language):
+        """
+        Assigns variables to the class upon initialization.
+        
+        Parameters
+        ----------
+        self : Planets
+        name : Column(String)
+        classification : Column(String)
+        average_height : Column(String)
+        average_lifespan : Column(String)
+        language : Column(String)
+        
+        Returns
+        -------
+        People
+        """
         self.name = name
         self.classification = classification
         self.average_height = average_height

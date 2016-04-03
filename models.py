a45f99f34eb580f313488f72fad861b2e5ee3c70
Clint Ascencio <clint.ascencio@datastax.com>
2016-04-02 19:56:25 -0500

    testing out models

--- models.py
+++ models.py
@@ -1,23 +1,27 @@
+import sys
 from sqlalchemy import *
+from flask import Flask, render_template, request, redirect, url_for
+from flask.ext.script import Manager
+from flask.ext.sqlalchemy import SQLAlchemy
+from app.app import db
 
-# ----
+# ----\
 # People
 # ----
-class People():
+class People(db.Model):
     """"People Model"""
 
     __tablename__ = 'people'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    gender = Column(String)
-    birth_year = Column(String)
-    height = Column(Integer)
-    mass = Column(Integer)
-    hair_color = Column(String)
-    eye_color = Column(String)
+    id = db.Column(db.Integer, primary_key=True, unique=True, index=True)
+    name = db.Column(db.String(256))
+    gender = db.Column(db.String(256))
+    birth_year = db.Column(db.String(256))
+    height = db.Column(db.Integer)
+    mass = db.Column(db.Integer)
+    hair_color = db.Column(db.String(256))
+    eye_color = db.Column(db.String(256))
 
-    
     def __init__(self, name, gender, birth_year, height, mass, hair_color, eye_color):
         """
         Assigns variables to the class upon initialization.
@@ -45,20 +49,33 @@ class People():
         self.hair_color = hair_color
         self.eye_color = eye_color
 
+    @property
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "name" : self.name,
+            "gender" : self.gender,
+            "birth year" : self.birth_year,
+            "height" : self.height,
+            "mass" : self.mass,
+            "hair color" : self.hair_color,
+            "eye color" : self.eye_color
+        }
+
 # ----
 # Planets
 # ----
-class Planets():
+class Planets(db.Model):
     """Planets Model"""
 
     __tablename__ = 'planets'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    climate = Column(String)
-    gravity = Column(String)
-    terrain = Column(String)
-    population = Column(Integer)
+    id = db.Column(db.Integer, primary_key=True, unique=True, index=True)
+    name = db.Column(db.String(256))
+    climate = db.Column(db.String(256))
+    gravity = db.Column(db.String(256))
+    terrain = db.Column(db.String(256))
+    population = db.Column(db.Integer)
     
     def __init__(self, name, climate, gravity, terrain, population): 
         """
@@ -83,21 +100,32 @@ class Planets():
         self.terrain = terrain
         self.population = population
 
+    @property
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "name" : self.name,
+            "climate" : self.climate,
+            "gravity" : self.gravity,
+            "terrain" : self.terrain,
+            "population" : self.population
+        }
+
+
 # ----
 # Species
 # ----
-class Species():
+class Species(db.Model):
     """Species Model"""
 
     __tablename__ = 'species'
 
-    #
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    classification = Column(String)
-    average_height = Column(String)
-    average_lifespan = Column(String)
-    language = Column(String)
+    id = db.Column(db.Integer, primary_key=True, unique=True, index=True)
+    name = db.Column(db.String)
+    classification = db.Column(db.String(256))
+    average_height = db.Column(db.String(256))
+    average_lifespan = db.Column(db.String(256))
+    language = db.Column(db.String(256))
 
     def __init__(self, name, classification, average_height, average_lifespan, language):
         """
@@ -121,3 +149,15 @@ class Species():
         self.average_height = average_height
         self.average_lifespan = average_lifespan
         self.language = language
+
+
+    @property
+    def serialize(self):
+        return {
+            "id" : self.id,
+            "name" : self.name,
+            "classification" : self.classification,
+            "average height" : self.average_height,
+            "average lifespan" : self.average_lifespan,
+            "language" : self.language
+        }

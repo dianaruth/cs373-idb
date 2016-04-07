872560d6a9b83fdd82750ad0af17600ec7da233a
Tony Serino <arserino@yahoo.com>
2016-04-07 18:17:31 -0500

    added slash for route

--- app/tests.py
+++ app/tests.py
@@ -317,7 +317,6 @@ class TestRESTfulAPI(TestCase):
     """
     def test_get_person_data_1(self):
         output = get_person_data(1)
-        print(str(output))
         assert output is not None and str(output).__contains__("[200 OK]") # JSON response successful
 
     def test_get_person_data_2(self):
@@ -406,4 +405,4 @@ class TestRESTfulAPI(TestCase):
         assert output is not None and str(output).__contains__("[200 OK]")  # JSON response successful
 
 if __name__ == '__main__':
-	unittest.main(verbosity=1)
+	unittest.main(verbosity=2)

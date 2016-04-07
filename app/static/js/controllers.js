1fbae35bd116afa7eeca86a38395ab268ec946a5
Tony Serino <arserino@yahoo.com>
2016-04-07 16:47:00 -0500

    tweaks

--- app/static/js/controllers.js
+++ app/static/js/controllers.js
@@ -87,7 +87,7 @@ returnOfTheAPIControllers.controller('AboutController', ['$scope',
             {
                 "name": "Tony Serino",
                 "photo": "tony.jpg",
-                "bio": "Originally from the wastelands of Siberia. My mother lost her life fighting ninjas and ebola at the same time when I was baby, and my other mother who's life choices you should respect sacrificed herself using love magic to protect me from the evil lord Voldermort. Hobbies mostly include survival and hiding from the CIA. They aren't looking for me, but if they ever start they're gonna in for a hell of a surprise. I am the last surviving heir to house Stark and am currently working on my next iron man suit to kill king Justin Beiber for starting the war in Westoros. HAIL HYDRA!",
+                "bio": "Originally from the wastelands of Siberia. My mother lost her life fighting ninjas and ebola at the same time when I was baby, and my other mother who's life choices you should respect sacrificed herself using love magic to protect me from the evil lord Voldermort. Hobbies mostly include survival and hiding from the CIA. They aren't looking for me, but if they ever start they're gonna be in for a hell of a surprise. I am the last surviving heir to house Stark and am currently working on my next iron man suit to kill king Justin Beiber for starting the war in Westoros. HAIL HYDRA!",
                 "responsibilities": ["Created models", "Initialized Flask project", "Set up Docker"],
                 "commits": 18,
                 "issues": 0,

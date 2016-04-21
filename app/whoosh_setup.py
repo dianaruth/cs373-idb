a8b59b2cb7fffe5a7998ae5e156d419d557fccbe
Clint Ascencio <clint.ascencio@datastax.com>
2016-04-21 16:40:33 -0500

    Added caseinsensitive support

--- app/whoosh_setup.py
+++ app/whoosh_setup.py
@@ -2,7 +2,7 @@ import os
 
 from whoosh import index, qparser
 from whoosh.fields import SchemaClass, TEXT, KEYWORD, ID, STORED
-from whoosh.qparser import MultifieldParser
+from whoosh.qparser import MultifieldParser, QueryParser
 from models import People, Species, Planets
 
 whoosh_index_path = "whoosh_index"
@@ -15,19 +15,18 @@ def create_whoosh_dir():
 
 class PeopleSchema(SchemaClass):
     id = ID(stored=True)
-    name = KEYWORD(stored=True)
-    gender = KEYWORD(stored=True)
-    birth_year = KEYWORD(stored=True)
-    height = KEYWORD(stored=True)
-    mass = KEYWORD(stored=True)
-    hair_color = KEYWORD(stored=True)
-    eye_color = KEYWORD(stored=True)
-    description = KEYWORD(stored=True)
-    image = KEYWORD(stored=True)
-    homeworld = KEYWORD(stored=True)
-    skin_color = KEYWORD(stored=True)
-    species = KEYWORD(stored=True)
-
+    name = KEYWORD(stored=True, lowercase=True)
+    gender = KEYWORD(stored=True, lowercase=True)
+    birth_year = KEYWORD(stored=True, lowercase=True)
+    height = KEYWORD(stored=True, lowercase=True)
+    mass = KEYWORD(stored=True, lowercase=True)
+    hair_color = KEYWORD(stored=True, lowercase=True)
+    eye_color = KEYWORD(stored=True, lowercase=True)
+    description = KEYWORD(stored=True, lowercase=True)
+    image = KEYWORD(stored=True, lowercase=True)
+    homeworld = KEYWORD(stored=True, lowercase=True)
+    skin_color = KEYWORD(stored=True, lowercase=True)
+    species = KEYWORD(stored=True, lowercase=True)
 
 def get_people_index():
     people_ix = index.create_in(whoosh_index_path, schema=PeopleSchema(),
@@ -40,31 +39,31 @@ def fill_people_index(ix):
     writer = ix.writer()
     people_list = People.query.all()
     for person in people_list:
-        writer.add_document(name=person.name.lower(),
-                            id=str(person.id).lower(),
-                            gender=person.gender.lower(),
-                            birth_year=person.birth_year.lower(),
-                            height=person.height.lower(),
-                            mass=person.mass.lower(),
-                            hair_color=person.hair_color.lower(),
-                            eye_color=person.eye_color.lower(),
-                            description=person.description.lower(),
-                            image=person.image.lower(),
-                            homeworld=person.homeworld.lower(),
-                            skin_color=person.skin_color.lower(),
-                            species=person.species.lower())
+        writer.add_document(name=person.name,
+                            id=str(person.id),
+                            gender=person.gender,
+                            birth_year=person.birth_year,
+                            height=person.height,
+                            mass=person.mass,
+                            hair_color=person.hair_color,
+                            eye_color=person.eye_color,
+                            description=person.description,
+                            image=person.image,
+                            homeworld=person.homeworld,
+                            skin_color=person.skin_color,
+                            species=person.species)
     writer.commit()
 
 
 class PlanetsSchema(SchemaClass):
     id = ID(stored=True)
-    name = KEYWORD(stored=True)
-    climate = KEYWORD(stored=True)
-    gravity = KEYWORD(stored=True)
-    terrain = KEYWORD(stored=True)
-    population = KEYWORD(stored=True)
-    description = KEYWORD(stored=True)
-    image = KEYWORD(stored=True)
+    name = KEYWORD(stored=True, lowercase=True)
+    climate = KEYWORD(stored=True, lowercase=True)
+    gravity = KEYWORD(stored=True, lowercase=True)
+    terrain = KEYWORD(stored=True, lowercase=True)
+    population = KEYWORD(stored=True, lowercase=True)
+    description = KEYWORD(stored=True, lowercase=True)
+    image = KEYWORD(stored=True, lowercase=True)
 
 
 def get_planets_index():
@@ -78,27 +77,27 @@ def fill_planets_index(ix):
     writer = ix.writer()
     planets_list = Planets.query.all()
     for planet in planets_list:
-        writer.add_document(name=planet.name.lower(),
+        writer.add_document(name=planet.name,
                             id=str(planet.id).lower(),
-                            climate=planet.climate.lower(),
-                            gravity=planet.gravity.lower(),
-                            terrain=planet.terrain.lower(),
-                            population=planet.population.lower(),
-                            description=planet.description.lower(),
-                            image=planet.image.lower())
+                            climate=planet.climate,
+                            gravity=planet.gravity,
+                            terrain=planet.terrain,
+                            population=planet.population,
+                            description=planet.description,
+                            image=planet.image)
     writer.commit()
 
 
 class SpeciesSchema(SchemaClass):
     id = ID(stored=True)
-    name = KEYWORD(stored=True)
-    classification = KEYWORD(stored=True)
-    average_height = KEYWORD(stored=True)
-    average_lifespan = KEYWORD(stored=True)
-    language = KEYWORD(stored=True)
-    description = KEYWORD(stored=True)
-    image = KEYWORD(stored=True)
-    homeworld = KEYWORD(stored=True)
+    name = KEYWORD(stored=True, lowercase=True)
+    classification = KEYWORD(stored=True, lowercase=True)
+    average_height = KEYWORD(stored=True, lowercase=True)
+    average_lifespan = KEYWORD(stored=True, lowercase=True)
+    language = KEYWORD(stored=True, lowercase=True)
+    description = KEYWORD(stored=True, lowercase=True)
+    image = KEYWORD(stored=True, lowercase=True)
+    homeworld = KEYWORD(stored=True, lowercase=True)
 
 
 def get_species_index():
@@ -112,21 +111,21 @@ def fill_species_index(ix):
     writer = ix.writer()
     species_list = Species.query.all()
     for species in species_list:
-        writer.add_document(name=species.name.lower(),
+        writer.add_document(name=species.name,
                             id=str(species.id).lower(),
-                            classification=species.classification.lower(),
-                            average_height=species.average_height.lower(),
-                            average_lifespan=species.average_lifespan.lower(),
-                            language=species.language.lower(),
-                            description=species.description.lower(),
-                            image=species.image.lower(),
-                            homeworld=species.homeworld.lower())
+                            classification=species.classification,
+                            average_height=species.average_height,
+                            average_lifespan=species.average_lifespan,
+                            language=species.language,
+                            description=species.description,
+                            image=species.image,
+                            homeworld=species.homeworld)
     writer.commit()
 
 
 def search_results(ix, search_query, fields, statement):
     qp = MultifieldParser(fields, schema=ix.schema, group=getattr(qparser, statement))
-    q = qp.parse(search_query.lower())
+    q = qp.parse(search_query)
     data = []
     with ix.searcher() as s:
         results = s.search(q)
